71f20c83a14fc9df257a1093227db951375988bd
Fredrik Johansson <jeanfredrik@gmail.com>
2024-12-01 17:50:08 +0100

    Disable new design for Contacts module until it is done

--- autoload/mod-contacts.php
+++ autoload/mod-contacts.php
@@ -1,24 +1,26 @@
 <?php
 
-add_filter(
-  "Modularity/Module/Template",
-  function ($template, $slug) {
-    if ($slug === "contacts") {
-      $template = "mod-contacts.blade.php";
-    }
-    return $template;
-  },
-  10,
-  2,
-);
+// XXX: This feature is disabled until it is finished
 
-// Modify social media choices to change Twitter to X
-add_filter("acf/load_field", function ($field) {
-  if ($field["key"] === "field_5bf6a737c1b6c") {
-    if (isset($field["choices"]["twitter"])) {
-      $field["choices"]["x"] = "X";
-      unset($field["choices"]["twitter"]);
-    }
-  }
-  return $field;
-});
+// add_filter(
+//   "Modularity/Module/Template",
+//   function ($template, $slug) {
+//     if ($slug === "contacts") {
+//       $template = "mod-contacts.blade.php";
+//     }
+//     return $template;
+//   },
+//   10,
+//   2,
+// );
+
+// // Modify social media choices to change Twitter to X
+// add_filter("acf/load_field", function ($field) {
+//   if ($field["key"] === "field_5bf6a737c1b6c") {
+//     if (isset($field["choices"]["twitter"])) {
+//       $field["choices"]["x"] = "X";
+//       unset($field["choices"]["twitter"]);
+//     }
+//   }
+//   return $field;
+// });
