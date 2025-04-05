fe64c3bc8544e7e0dd360d81c0db90e76576febb
Fredrik Johansson <jeanfredrik@gmail.com>
2025-04-05 21:40:36 +0200

    chore: Release v24.113.7

--- municipio-extended.php
+++ municipio-extended.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Municipio Extended
  * Description: Adds more features to Municipio.
- * Version: 24.113.6
+ * Version: 24.113.7
  * Author: Whitespace Dev
  * Text Domain: municipio-extended
  * Domain Path: /languages/
